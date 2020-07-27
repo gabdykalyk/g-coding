5e80913c51cdb57d3da890ad22204005e17780a4
gabdykalyk <gabdykalyk@gmail.com>
2020-07-27 18:47:41 +0600

    ad fixed

--- scss/main.js
+++ scss/main.js
@@ -6,3 +6,20 @@ btn.addEventListener('click', (event) => {
     back.classList.toggle('active')
     body.classList.toggle('lock')
 })
+
+function escape(div) {
+    div.classList.add('escape')
+}
+
+let close = document.querySelector('#close')
+
+close.addEventListener('click', (event) => {
+    event.stopPropagation()
+    setTimeout(escape(ad), 1000)
+})
+
+let btnLink = document.querySelector('.btn__link')
+btnLink.addEventListener('click', (event) => {
+    event.stopPropagation()
+    setTimeout(escape(ad), 1000)
+})
